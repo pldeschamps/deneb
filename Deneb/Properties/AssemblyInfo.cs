3464688260a10095f77946cb220ff014b92745e3
jdd <jdd.deschamps@gmail.com>
2017-01-01 14:01:03 +0100

    changed license to GNU license

--- Deneb/Properties/AssemblyInfo.cs
+++ Deneb/Properties/AssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("-")]
 [assembly: AssemblyProduct("Deneb")]
-[assembly: AssemblyCopyright("Copyright © Géraud Benazet and Pierre-Louis Deschamps")]
-[assembly: AssemblyTrademark("Eurekode")]
+[assembly: AssemblyCopyright("GNU License Géraud Benazet and Pierre-Louis Deschamps")]
+[assembly: AssemblyTrademark("-")]
 [assembly: AssemblyCulture("")]
 
 // L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.5")]
-[assembly: AssemblyFileVersion("1.0.0.5")]
+[assembly: AssemblyVersion("1.0.0.6")]
+[assembly: AssemblyFileVersion("1.0.0.6")]
